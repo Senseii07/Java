b6ce57ea769773bb219f3b9598ffe9f62686e7c1
Senseii07 <magarzayn456@gmail.com>
2025-06-27 09:14:49 +0545

    Third commit

--- MainApp.java
+++ MainApp.java
@@ -1,5 +1,27 @@
 public class MainApp{
     public static void main(String[] args){
-        ClassName cn = new ClassName();
+        Dog dog = new Dog();
+        dog.speak();
     }
+}
+class Animal{
+    public void speak() {
+        System.out.println("Animal speaks");
+    }
+    public void eat(){
+        System.out.println("Animal eats");
+    }
+}
+class Dog extends Animal{
+    @Override
+    public void speak() {
+        System.out.println("Bark");
+    }
+class Cat extends Animal{
+    @Override
+    public void speak(){
+        System.out.println("Meow");
+    }
+}
+
 }
